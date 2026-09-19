46415149669c69d00c10974e0a7dbf016fa638de
chesi44 <danielgerman13@hotmail.com>
2026-09-19 11:30:28 -0300

    hice carrusel

--- main.js
+++ main.js
@@ -1,4 +1,49 @@
-//INDEX !
+//INDEX
+const trabajos = [
+    {
+        tipo: "imagen",
+        src: "img/corte1.jpeg"
+    },
+    {
+        tipo: "imagen",
+        src: "img/corte5.jpeg"
+    },
+    {
+        tipo: "imagen",
+        src: "img/corte3.jpeg"
+    },
+    {
+        tipo: "imagen",
+        src: "img/video1.mp4"
+    }
+];
+const contenedorTrabajo = document.getElementById("contenedorTrabajos");
+trabajos.forEach((trabajo) => {
+    const imagen = document.createElement("img");
+    imagen.src = trabajo.src;
+    imagen.alt = "Trabajo realizado en JM Barbería";
+    imagen.classList.add("imagen-trabajo");
+
+    contenedorTrabajo.appendChild(imagen);
+});
+const botonSiguiente = document.getElementById("siguiente");
+const botonAnterior = document.getElementById("anterior");
+botonSiguiente.addEventListener("click", () => {
+    const imagen = document.querySelector(".imagen-trabajo");
+    const distancia = imagen.offsetWidth + 20;
+    contenedorTrabajo.scrollBy({
+        left: distancia,
+        behavior: "smooth"
+    });
+});
+botonAnterior.addEventListener("click", () => {
+    const imagen = document.querySelector(".imagen-trabajo");
+    const distancia = imagen.offsetWidth + 20;    
+    contenedorTrabajo.scrollBy({
+        left: -distancia,
+        behavior: "smooth"
+    });
+});
 const horariosDisponibles = document.getElementById("horariosDisponibles");
 let duracionSeleccionada = 0;
 const botonesServicio = document.querySelectorAll(".boton-servicio");
